9201c5423c870c5387c8c475f7b8cbeaaa82ec9f
Indira Williams <indirajamurath@gmail.com>
2018-09-17 12:50:28 -0400

    emeny and player moving around canvas

--- js/app.js
+++ js/app.js
@@ -1,17 +1,36 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x; //0
+    this.y = y; //50
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
 };
+
+// if (this.bearing === 'north'){
+//   this.coordinates[1] += 1
+// }
+// else if(this.bearing === 'east'){
+//   this.coordinates[0] += 1
+// }
+// else if(this.bearing === 'south'){
+//   this.coordinates[1] -= 1
+// }
+// else {
+//   this.coordinates[0] -= 1
+// }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+  //console.log('dt:', dt)
     // You should multiply any movement by the dt parameter
+
+      this.x < 500 ? this.x = this.x + (this.speed*dt) : this.x = 0
     // which will ensure the game runs at the same speed for
     // all computers.
 };
@@ -24,13 +43,45 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(){
+  this.x = 200;
+  this.y = 440;
 
+  this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(key){
+  if (key === 'up'){
+    this.y -= 10
+  }
+  else if (key === 'down'){
+    this.y < 440 ? this.y += 10 : this.y = 440
+  }
+  else if (key === 'right'){
+    this.x += 10
+  }
+  else if (key === 'left') {
+    this.x -= 10
+  }
+};
+
+Player.prototype.render = function(){
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key){
+  this.update(key)
+};
+
+var player = new Player()
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+let bug1 = new Enemy(0, 50, 50); //(x, y, speed)
+let bug2 = new Enemy(0, 150, 30);
+let bug3 = new Enemy(0, 250, 70);
+var allEnemies = [bug1, bug2, bug3];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
