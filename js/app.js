23de346ad6a727eda07faeabf74bfa9ed8f581b2
Indira Williams <indirajamurath@gmail.com>
2018-09-18 09:52:38 -0400

    got collision detection

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ var Enemy = function(x,y,speed) {
     this.x = x; //0
     this.y = y; //50
     this.speed = speed;
+    this.width = 60;
+    this.height = 70;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -15,12 +17,18 @@ var Enemy = function(x,y,speed) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-  //console.log('dt:', dt)
-    // You should multiply any movement by the dt parameter
+    //multiply any movement by the dt parameter
       this.x < 540 ? this.x = this.x + (this.speed*dt) : this.x = 0
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    //which will ensure the game runs at the same speed for all computers.
+    //this.y === player.y ? console.log('yikes') : null
+    if (this.x < player.x + player.width  && this.x + this.width  > player.x &&
+    		this.y < player.y + player.height && this.y + this.height > player.y) {
+
+          player.reset()
+    }
 };
+
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -32,29 +40,32 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 var Player = function(){
   this.x = 200;
-  this.y = 440;
+  this.y = 400;
+  this.width = 60;
+  this.height = 70;
 
-  this.sprite = 'images/char-boy.png';
+  this.sprite = 'images/char-cat-girl.png';
 };
 
 Player.prototype.reset = function(){
   this.x = 200;
-  this.y = 440;
+  this.y = 400;
 }
 
 Player.prototype.update = function(key){
   if (key === 'up'){
-    this.y > -20 ? this.y -= 20 : this.y = -20
+    this.y > -20 ? this.y -= 30 : this.y = -20
   }
   else if (key === 'down'){
-    this.y < 440 ? this.y += 20 : this.y = 440
+    this.y < 400 ? this.y += 30 : this.y = 400
   }
   else if (key === 'right'){
-    this.x < 420 ? this.x += 20 : this.x = 420
+    this.x < 420 ? this.x += 30 : this.x = 420
   }
   else if (key === 'left') {
-    this.x > -20? this.x -= 20 : this.x = -20
+    this.x > -20? this.x -= 30 : this.x = -20
   }
+
   return this.y
 };
 
@@ -63,8 +74,10 @@ Player.prototype.render = function(){
 };
 
 Player.prototype.handleInput = function(key){
+
   this.update(key)
-  this.update(key) === -20 ? this.reset() : null
+  //console.log(this.y)
+  this.y === -20 ? this.reset() : null
 };
 
 var player = new Player()
@@ -72,9 +85,9 @@ var player = new Player()
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let bug1 = new Enemy(0, 60, 60); //(x, y, speed)
-let bug2 = new Enemy(-10, 150, 140);
-let bug3 = new Enemy(0, 230, 100);
+let bug1 = new Enemy(0, 70, 60); //(x, y, speed)
+let bug2 = new Enemy(0, 130, 140);
+let bug3 = new Enemy(0, 220, 100);
 var allEnemies = [bug1, bug2, bug3];
 
 // This listens for key presses and sends the keys to your
